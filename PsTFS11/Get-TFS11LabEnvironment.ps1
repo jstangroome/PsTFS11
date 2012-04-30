39c8644533eecc4aac1c7ef42f20be7ce8b53d91
Jason Stangroome <jason@codeassassin.com>
2012-04-30 10:39:46 +1000

    Get lab environment by LabUri

--- PsTFS11/Get-TFS11LabEnvironment.ps1
+++ PsTFS11/Get-TFS11LabEnvironment.ps1
@@ -1,11 +1,20 @@
 function Get-TFS11LabEnvironment {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='Name')]
     [OutputType([Microsoft.TeamFoundation.Lab.Client.LabEnvironment, Microsoft.TeamFoundation.Lab.Client, Version=11.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a])]
     param (
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(ParameterSetName='Name', Mandatory=$true, ValueFromPipeline=$true, Position=0)]
         [object[]]
         $Project,
+
+        [Parameter(ParameterSetName='Uri', Mandatory=$true, Position=0)]
+        [Uri]
+        $LabUri,
+
+        [Parameter(ParameterSetName='Name')]
+        [Parameter(ParameterSetName='Uri', Mandatory=$true)]
         $Collection,
+
+        [Parameter(ParameterSetName='Name')]
         [Microsoft.TeamFoundation.Lab.Client.LabEnvironmentDisposition, Microsoft.TeamFoundation.Lab.Client, Version=11.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a]
         $Disposition
     )
@@ -14,6 +23,11 @@ function Get-TFS11LabEnvironment {
         
         if ($Collection -is [string] -or $Collection -is [Uri]) {
             $Collection = Get-TFS11TeamProjectCollection -CollectionUri $Collection
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq 'Uri') {
+            $LabService = $Collection.GetService([Microsoft.TeamFoundation.Lab.Client.LabService])
+            return ,$LabService.GetLabEnvironment($LabUri)
         }
 
         foreach ($SingleProject in $Project) {
